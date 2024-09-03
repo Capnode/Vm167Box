8f9f6de55c9d0594a0023714a2de59e91b9d03a4
Carl Christerson <carlcnx@outlook.com>
2024-09-03 14:29:06 +0200

    Improve theme handling and element referencing
    - Consolidated `Shell.FlyoutBackgroundColor` formatting in `AppShell.xaml` for better readability.
    - Added `x:Name="_scopeView"` to `oxy:PlotView` in `ScopePage.xaml`.
    - Imported `OxyPlot` and `Vm167Box.ViewModels` namespaces in `ScopePage.xaml.cs`.
    - Subscribed to `RequestedThemeChanged` event in `ScopePage` constructor.
    - Added `OnRequestedThemeChanged` method to update plot model colors based on theme.

--- Vm167Box/Views/ScopePage.xaml.cs
+++ Vm167Box/Views/ScopePage.xaml.cs
@@ -1,3 +1,4 @@
+using OxyPlot;
 using Vm167Box.ViewModels;
 
 namespace Vm167Box.Views;
@@ -8,5 +9,38 @@ public partial class ScopePage : ContentPage
     {
         InitializeComponent();
         BindingContext = viewModel;
+
+        if (Application.Current != null)
+        {
+            Application.Current.RequestedThemeChanged += OnRequestedThemeChanged;
+        }
+    }
+
+    private void OnRequestedThemeChanged(object? sender, AppThemeChangedEventArgs e)
+    {
+        var model = _scopeView.Model;
+        var foreground = e.RequestedTheme == AppTheme.Light
+            ? OxyColor.FromRgb(32, 32, 32)
+            : OxyColors.WhiteSmoke;
+
+        if (model.TextColor != OxyColors.Transparent)
+        {
+            model.TextColor = foreground;
+        }
+
+        foreach (var axis in model.Axes)
+        {
+            if (axis.TicklineColor != OxyColors.Transparent)
+            {
+                axis.TicklineColor = foreground;
+            }
+
+            if (axis.AxislineColor != OxyColors.Transparent)
+            {
+                axis.AxislineColor = foreground;
+            }
+        }
+
+        model.InvalidatePlot(false);
     }
 }
