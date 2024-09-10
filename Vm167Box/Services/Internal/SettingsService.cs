281aedde212ed6be7ea224b0261fb650cb6d9f39
Carl Christerson <carlcnx@outlook.com>
2024-09-10 15:53:09 +0200

    Trigger Update event on property changes in SettingsService
    Modified setters in SettingsService.cs to invoke the Update event
    after setting property values. This affects properties such as
    AppTheme, Analog1Name, Analog1Unit, Analog1Point1, Analog1Value1,
    Analog1Point2, Analog1Value2, Analog2Name, Analog2Unit, Analog2Point1,
    Analog2Value1, Analog2Point2, Analog2Value2, Analog3Name, Analog3Unit,
    Analog3Point1, Analog3Value1, Analog3Point2, Analog3Value2, Analog4Name,
    Analog4Unit, Analog4Point1, Analog4Value1, Analog4Point2, Analog4Value2,
    Analog5Name, Analog5Unit, Analog5Point1, Analog5Value1, Analog5Point2,
    Analog5Value2, Pwm1Name, Pwm1Unit, Pwm1Point1, Pwm1Value1, Pwm1Point2,
    Pwm1Value2, Pwm2Name, Pwm2Unit, Pwm2Point1, Pwm2Value1, Pwm2Point2,
    and Pwm2Value2. This ensures that changes to these properties trigger
    the Update event, allowing other parts of the application to respond.

--- Vm167Box/Services/Internal/SettingsService.cs
+++ Vm167Box/Services/Internal/SettingsService.cs
@@ -19,259 +19,431 @@ internal class SettingsService : ISettingsService
     public AppTheme AppTheme
     {
         get => _settings.Get(nameof(AppTheme), Application.Current?.PlatformAppTheme ?? AppTheme.Unspecified);
-        set => _settings.Set(nameof(AppTheme), (int)value);
+        set
+        {
+            _settings.Set(nameof(AppTheme), (int)value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog1Name
     {
         get => _settings.Get(nameof(Analog1Name), AppResources.Analog1);
-        set => _settings.Set(nameof(Analog1Name), value);
+        set
+        {
+            _settings.Set(nameof(Analog1Name), value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog1Unit
     {
         get => _settings.Get(nameof(Analog1Unit), string.Empty);
-        set => _settings.Set(nameof(Analog1Unit), value);
+        set
+        {
+            _settings.Set(nameof(Analog1Unit), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog1Point1
     {
         get => _settings.Get(nameof(Analog1Point1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog1Point1), value);
+        set
+        {
+            _settings.Set(nameof(Analog1Point1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog1Value1
     {
         get => _settings.Get(nameof(Analog1Value1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog1Value1), value);
+        set
+        {
+            _settings.Set(nameof(Analog1Value1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog1Point2
     {
         get => _settings.Get(nameof(Analog1Point2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog1Point2), value);
+        set
+        {
+            _settings.Set(nameof(Analog1Point2), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog1Value2
     {
         get => _settings.Get(nameof(Analog1Value2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog1Value2), value);
+        set
+        {
+            _settings.Set(nameof(Analog1Value2), value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog2Name
     {
         get => _settings.Get(nameof(Analog2Name), AppResources.Analog2);
-        set => _settings.Set(nameof(Analog2Name), value);
+        set
+        {
+            _settings.Set(nameof(Analog2Name), value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog2Unit
     {
         get => _settings.Get(nameof(Analog2Unit), string.Empty);
-        set => _settings.Set(nameof(Analog2Unit), value);
+        set
+        {
+            _settings.Set(nameof(Analog2Unit), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog2Point1
     {
         get => _settings.Get(nameof(Analog2Point1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog2Point1), value);
+        set
+        {
+            _settings.Set(nameof(Analog2Point1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog2Value1
     {
         get => _settings.Get(nameof(Analog2Value1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog2Value1), value);
+        set
+        {
+            _settings.Set(nameof(Analog2Value1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog2Point2
     {
         get => _settings.Get(nameof(Analog2Point2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog2Point2), value);
+        set
+        {
+            _settings.Set(nameof(Analog2Point2), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog2Value2
     {
         get => _settings.Get(nameof(Analog2Value2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog2Value2), value);
+        set
+        {
+            _settings.Set(nameof(Analog2Value2), value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog3Name
     {
         get => _settings.Get(nameof(Analog3Name), AppResources.Analog3);
-        set => _settings.Set(nameof(Analog3Name), value);
+        set
+        {
+            _settings.Set(nameof(Analog3Name), value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog3Unit
     {
         get => _settings.Get(nameof(Analog3Unit), string.Empty);
-        set => _settings.Set(nameof(Analog3Unit), value);
+        set
+        {
+            _settings.Set(nameof(Analog3Unit), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog3Point1
     {
         get => _settings.Get(nameof(Analog3Point1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog3Point1), value);
+        set
+        {
+            _settings.Set(nameof(Analog3Point1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog3Value1
     {
         get => _settings.Get(nameof(Analog3Value1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog3Value1), value);
+        set
+        {
+            _settings.Set(nameof(Analog3Value1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog3Point2
     {
         get => _settings.Get(nameof(Analog3Point2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog3Point2), value);
+        set
+        {
+            _settings.Set(nameof(Analog3Point2), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog3Value2
     {
         get => _settings.Get(nameof(Analog3Value2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog3Value2), value);
+        set
+        {
+            _settings.Set(nameof(Analog3Value2), value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog4Name
     {
         get => _settings.Get(nameof(Analog4Name), AppResources.Analog4);
-        set => _settings.Set(nameof(Analog4Name), value);
+        set
+        {
+            _settings.Set(nameof(Analog4Name), value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog4Unit
     {
         get => _settings.Get(nameof(Analog4Unit), string.Empty);
-        set => _settings.Set(nameof(Analog4Unit), value);
+        set
+        {
+            _settings.Set(nameof(Analog4Unit), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog4Point1
     {
         get => _settings.Get(nameof(Analog4Point1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog4Point1), value);
+        set
+        {
+            _settings.Set(nameof(Analog4Point1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog4Value1
     {
         get => _settings.Get(nameof(Analog4Value1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog4Value1), value);
+        set
+        {
+            _settings.Set(nameof(Analog4Value1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog4Point2
     {
         get => _settings.Get(nameof(Analog4Point2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog4Point2), value);
+        set
+        {
+            _settings.Set(nameof(Analog4Point2), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog4Value2
     {
         get => _settings.Get(nameof(Analog4Value2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog4Value2), value);
+        set
+        {
+            _settings.Set(nameof(Analog4Value2), value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog5Name
     {
         get => _settings.Get(nameof(Analog5Name), AppResources.Analog5);
-        set => _settings.Set(nameof(Analog5Name), value);
+        set
+        {
+            _settings.Set(nameof(Analog5Name), value);
+            Update?.Invoke();
+        }
     }
 
     public string Analog5Unit
     {
         get => _settings.Get(nameof(Analog5Unit), string.Empty);
-        set => _settings.Set(nameof(Analog5Unit), value);
+        set
+        {
+            _settings.Set(nameof(Analog5Unit), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog5Point1
     {
         get => _settings.Get(nameof(Analog5Point1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog5Point1), value);
+        set
+        {
+            _settings.Set(nameof(Analog5Point1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog5Value1
     {
         get => _settings.Get(nameof(Analog5Value1), (double)IVm167.AnalogMin);
-        set => _settings.Set(nameof(Analog5Value1), value);
+        set
+        {
+            _settings.Set(nameof(Analog5Value1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog5Point2
     {
         get => _settings.Get(nameof(Analog5Point2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog5Point2), value);
+        set
+        {
+            _settings.Set(nameof(Analog5Point2), value);
+            Update?.Invoke();
+        }
     }
 
     public double Analog5Value2
     {
         get => _settings.Get(nameof(Analog5Value2), (double)IVm167.AnalogMax);
-        set => _settings.Set(nameof(Analog5Value2), value);
+        set
+        {
+            _settings.Set(nameof(Analog5Value2), value);
+            Update?.Invoke();
+        }
     }
 
     public string Pwm1Name
     {
         get => _settings.Get(nameof(Pwm1Name), AppResources.Pwm1);
-        set => _settings.Set(nameof(Pwm1Name), value);
+        set
+        {
+            _settings.Set(nameof(Pwm1Name), value);
+            Update?.Invoke();
+        }
     }
 
     public string Pwm1Unit
     {
         get => _settings.Get(nameof(Pwm1Unit), string.Empty);
-        set => _settings.Set(nameof(Pwm1Unit), value);
+        set
+        {
+            _settings.Set(nameof(Pwm1Unit), value);
+            Update?.Invoke();
+        }
     }
 
     public double Pwm1Point1
     {
         get => _settings.Get(nameof(Pwm1Point1), (double)IVm167.PwmMin);
-        set => _settings.Set(nameof(Pwm1Point1), value);
+        set
+        {
+            _settings.Set(nameof(Pwm1Point1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Pwm1Value1
     {
         get => _settings.Get(nameof(Pwm1Value1), (double)IVm167.PwmMin);
-        set => _settings.Set(nameof(Pwm1Value1), value);
+        set
+        {
+            _settings.Set(nameof(Pwm1Value1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Pwm1Point2
     {
         get => _settings.Get(nameof(Pwm1Point2), (double)IVm167.PwmMax);
-        set => _settings.Set(nameof(Pwm1Point2), value);
+        set
+        {
+            _settings.Set(nameof(Pwm1Point2), value);
+            Update?.Invoke();
+        }
     }
 
     public double Pwm1Value2
     {
         get => _settings.Get(nameof(Pwm1Value2), (double)IVm167.PwmMax);
-        set => _settings.Set(nameof(Pwm1Value2), value);
+        set
+        {
+            _settings.Set(nameof(Pwm1Value2), value);
+            Update?.Invoke();
+        }
     }
 
     public string Pwm2Name
     {
         get => _settings.Get(nameof(Pwm2Name), AppResources.Pwm2);
-        set => _settings.Set(nameof(Pwm2Name), value);
+        set
+        {
+            _settings.Set(nameof(Pwm2Name), value);
+            Update?.Invoke();
+        }
     }
 
     public string Pwm2Unit
     {
         get => _settings.Get(nameof(Pwm2Unit), string.Empty);
-        set => _settings.Set(nameof(Pwm2Unit), value);
+        set
+        {
+            _settings.Set(nameof(Pwm2Unit), value);
+            Update?.Invoke();
+        }
     }
 
     public double Pwm2Point1
     {
         get => _settings.Get(nameof(Pwm2Point1), (double)IVm167.PwmMin);
-        set => _settings.Set(nameof(Pwm2Point1), value);
+        set
+        {
+            _settings.Set(nameof(Pwm2Point1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Pwm2Value1
     {
         get => _settings.Get(nameof(Pwm2Value1), (double)IVm167.PwmMin);
-        set => _settings.Set(nameof(Pwm2Value1), value);
+        set
+        {
+            _settings.Set(nameof(Pwm2Value1), value);
+            Update?.Invoke();
+        }
     }
 
     public double Pwm2Point2
     {
         get => _settings.Get(nameof(Pwm2Point2), (double)IVm167.PwmMax);
-        set => _settings.Set(nameof(Pwm2Point2), value);
+        set
+        {
+            _settings.Set(nameof(Pwm2Point2), value);
+            Update?.Invoke();
+        }
     }
 
     public double Pwm2Value2
     {
         get => _settings.Get(nameof(Pwm2Value2), (double)IVm167.PwmMax);
-        set => _settings.Set(nameof(Pwm2Value2), value);
+        set
+        {
+            _settings.Set(nameof(Pwm2Value2), value);
+            Update?.Invoke();
+        }
     }
 
     public void Reset()
