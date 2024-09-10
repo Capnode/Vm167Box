6b56c341988fa7127b331eede9ff8fc8d125a190
Carl Christerson <carlcnx@outlook.com>
2024-09-10 14:50:17 +0200

    Refactor validation behaviors and add new resources
    Updated `SettingsPage.xaml` to remove `toolkit` namespace and `ContentPage.Resources` section, replacing numeric validation styles with attached properties from new `AnalogBehavior` and `PwmBehavior` classes. Added `AnalogBehavior.cs` and `PwmBehavior.cs` to define these behaviors. Introduced `Resource.cs` with `FindResource` extension method for `ResourceDictionary`.

---
+++ Vm167Box/Behaviors/PwmBehavior.cs
@@ -0,0 +1,51 @@
+using CommunityToolkit.Maui.Behaviors;
+using Vm167Lib;
+
+namespace Vm167Box.Behaviors
+{
+    public static class PwmBehavior
+    {
+        public static readonly BindableProperty NumericValidationBehaviorProperty =
+            BindableProperty.CreateAttached(
+                "NumericValidationBehavior",
+                typeof(bool),
+                typeof(PwmBehavior),
+                false,
+                propertyChanged: OnNumericValidationBehaviorChanged);
+
+        public static bool GetNumericValidationBehavior(BindableObject view)
+        {
+            return (bool)view.GetValue(NumericValidationBehaviorProperty);
+        }
+
+        public static void SetNumericValidationBehavior(BindableObject view, bool value)
+        {
+            view.SetValue(NumericValidationBehaviorProperty, value);
+        }
+
+        private static void OnNumericValidationBehaviorChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is Entry entry && newValue is bool isEnabled && isEnabled)
+            {
+                var invalidStyle = new Style(typeof(Entry))
+                {
+                    Setters = { new Setter { Property = Entry.TextColorProperty, Value = Colors.Red } }
+                };
+
+                var validStyle = new Style(typeof(Entry))
+                {
+                    Setters = { new Setter { Property = Entry.TextColorProperty, Value = Colors.Green } }
+                };
+
+                entry.Behaviors.Add(new NumericValidationBehavior
+                {
+                    Flags = ValidationFlags.ValidateOnValueChanged,
+                    InvalidStyle = invalidStyle,
+                    MinimumValue = IVm167.PwmMin,
+                    MaximumValue = IVm167.PwmMax,
+                    ValidStyle = validStyle
+                });
+            }
+        }
+    }
+}
---
+++ Vm167Box/Helpers/Resource.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+
+namespace Vm167Box.Helpers
+{
+    public static class Resource
+    {
+        public static object? FindResource(this ResourceDictionary mainResourceDictionary, string resourceKey)
+        {
+            if (mainResourceDictionary == null)
+            {
+                throw new ArgumentNullException(nameof(mainResourceDictionary));
+            }
+
+            var mergedResources = mainResourceDictionary
+                .GetType()
+                .GetProperty("MergedResources", BindingFlags.Instance | BindingFlags.NonPublic)
+                ?.GetValue(mainResourceDictionary);
+
+            if (mergedResources == null)
+            {
+                return null;
+            }
+
+            var dict = ((IEnumerable<KeyValuePair<string, object>>)mergedResources)
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            if (dict?.ContainsKey(resourceKey) == true)
+            {
+                return dict[resourceKey];
+            }
+
+            return null;
+        }
+    }
+}
